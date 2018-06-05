76ef31874da4f115b523f48240d0a2be77999b3a
Nigel Reoch <nreoch@Nigels-MacBook-Pro.local>
2018-06-05 17:50:32 -0400

    02_package

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nreoch25/go-training/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
